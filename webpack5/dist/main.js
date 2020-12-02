085923b0b5bea7f52baf87f21621d13292a6af3b
topaz <topaz_h@163.com>
2020-12-02 23:28:23 +0800

    feat(webpack5): new Project

---
+++ webpack5/dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+console.log(111)
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
